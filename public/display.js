ccbb8868fb94f947f83f4ad3825879cc81cb2ef8
taoyexiang <ytao04@students.cs.ubc.ca>
2023-11-27 18:26:05 -0800

    implemented and connected display

--- public/display.js
+++ public/display.js
@@ -1,32 +1,39 @@
-function handleDisplayAPIResponse(responseHandle) {
-    try {
-        const tableArea = document.getElementById("result-table");
-        responseHandle.forEach(eachRow => {
-            const div = document.createElement('div');
-            for (const attribute in eachRow) {
-                div.innerHTML += `<b>${attribute}:</b> ${eachRow[attribute]}`;
-            }
-        tableArea.appendChild(div);
-        })
-    } catch (error) {
-        console.error("Error: " + error)
-    }
-
-}
+// function handleDisplayAPIResponse(responseHandle) {
+//     try {
+//         const tableArea = document.getElementById("result-table");
+//         responseHandle.forEach(eachRow => {
+//             const div = document.createElement('div');
+//             for (const attribute in eachRow) {
+//                 div.innerHTML += `<b>${attribute}:</b> ${eachRow[attribute]}`;
+//             }
+//         tableArea.appendChild(div);
+//         })
+//     } catch (error) {
+//         console.error("Error: " + error)
+//     }
+//
+// }
 
 async function displayTable() {
     const selectedDropDown = document.getElementById("DropDown").value;
-    const projections = [];
-    const filters = "";
-    const displayParameters = {selectedDropDown, projections, filters};
-    const response = await fetch('/demotable', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(displayParameters),
-    })
-        .then(response => response.json())
-        .then(responseHandle => handleDisplayAPIResponse(responseHandle))
-        .catch(error => console.error('Error: ' + error));
+    const tableElement = document.getElementById('displayTable')
+    const tableBody = tableElement.querySelector('tbody');
+    const response = await fetch(`/display-${selectedDropDown.toLowerCase()}`, {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const tableContent = responseData.data;
+
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    tableContent.forEach(user => {
+        const row = tableBody.insertRow();
+        user.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        });
+    });
 }
